671f2cbe2345d8967c0ac96b924e46de273b4450
luke-ackerman <79563871+luke-ackerman@users.noreply.github.com>
2023-03-27 17:14:24 -0400

    Updated cpp files and updated notebook

--- screen_average/hls_impl/average_screen.cpp
+++ screen_average/hls_impl/average_screen.cpp
@@ -2,34 +2,42 @@
 
 void average_screen(
 		color_array *result,
-		color_array *input_array
+		volatile color_array* input_array
 )
 {
-#pragma HLS INTERFACE s_axilite port=return
-#pragma HLS INTERFACE s_axilite port=result
-#pragma HLS INTERFACE m_axi port=input_array offset=slave
+#pragma HLS INTERFACE mode=s_axilite port=return
+#pragma HLS INTERFACE mode=s_axilite port=result
+#pragma HLS INTERFACE mode=m_axi port=input_array depth=50 offset=slave
 
 	int i;
-	int j;
-	//color_array buff[IMAGE_WIDTH*IMAGE_HEIGHT];
+	color_array num;
+	color_array max = 0;
+	int buff[IMAGE_LENGTH];
 
 	//memcpy creates a burst access to memory
 	//multiple calls of memcpy cannot be pipelined and will be scheduled sequentially
 	//memcpy requires a local buffer to store the results of the memory transaction
-	//memcpy(buff,(const color_array*)input_array,IMAGE_WIDTH*IMAGE_HEIGHT*sizeof(color_array));
+	memcpy(buff,(const color_array*)input_array,IMAGE_LENGTH*sizeof(color_array*));
 
 Find_Color_Loop:
-	for(i = 0; i < IMAGE_WIDTH*IMAGE_HEIGHT; i++) {
-			color255[input_array[i]] += 1;
+	for(i = 0; i < IMAGE_LENGTH; i++) {
+				num = input_array[i];
+				color255[num] += 1;
 		}
 
-	int max = color255[0];
 Find_Max_Loop:
-	for(i = 1; i < 256; i++) {
-		if(color255[i] > max)
-			max = color255[i];
+	for(i = 0; i < 256; i++) {
+		if(i ==0) {
+			max = i;
+		}
+		else {
+			if(color255[i] > color255[max]) {
+				max = i;
+			}
+		}
 	}
 
 	*result = max;
+	memcpy((color_array *)input_array, &max, sizeof(color_array));
 
 }
--- screen_average/hls_impl/average_screen.h
+++ screen_average/hls_impl/average_screen.h
@@ -5,16 +5,15 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef ap_int<8> color_array;
+typedef ap_uint<8> color_array;
 
-#define IMAGE_WIDTH 711
-#define IMAGE_HEIGHT 360
+#define IMAGE_LENGTH 255960
 
 int color255[256] = {0};
 
-void average_screen(
-		color_array *result,
-		color_array input_array[IMAGE_WIDTH][IMAGE_HEIGHT]
-);
+// void average_screen(
+	//	color_array *result,
+		// color_array input_array[IMAGE_WIDTH][IMAGE_HEIGHT]
+//);
 
 #endif
