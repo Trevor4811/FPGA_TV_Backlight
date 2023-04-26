af088fe7587a604b8b4ea1b9886ef520932de83a
luke-ackerman <79563871+luke-ackerman@users.noreply.github.com>
2023-04-26 19:12:22 -0400

    Add files via upload

--- screen_average/hls_impl_final/average_screen2.cpp
+++ screen_average/hls_impl_final/average_screen2.cpp
@@ -9,37 +9,38 @@ using namespace std;
 void example(hls::stream< ap_axis<32,2,5,6> > &A,
 	     hls::stream< ap_axis<32,2,5,6> > &B)
 {
-#pragma HLS INTERFACE axis port=A
-#pragma HLS INTERFACE axis port=B
 #pragma HLS INTERFACE s_axilite port=return
+#pragma HLS INTERFACE mode=axis port=A
+#pragma HLS INTERFACE mode=axis port=B
 
-	int i;
-	int num;
-	int max = 0;
+	static int i;
+	static int num;
+	static int max = 0;
 
 	ap_axis<32,2,5,6> tmp;
+
     while(1)
     {
 	A.read(tmp);
 
-	Find_Color_Loop:
-		for(i = 0; i < IMAGE_LENGTH; i++) {
-			num = tmp.data.to_int();
-			color255[num] += 1;
-		}
+	num = tmp.data.to_int();
+	color255[num] += 1;
+	// cout << "added value: " << num << " " << color255[num] << endl;
 
+    if(tmp.last)
+    	{
+        	break;
+    	}
 
-		if(tmp.last) {
-			Find_Max_Loop:
-			for(i = 0; i < 256; i++) {
-				if(color255[i] > color255[max]) {
-					max = i;
-				}
-			}
-			tmp.data = max;
-			B.write(tmp);
-			break;
-		}
-    }
+	}
+	 for(i = 0; i < 256; i++) {
+		 if(color255[i] > color255[max]) {
+			 max = i;
+		 	 }
+
+	 	 }
+	   	tmp.data = max;
+	    B.write(tmp);
+
 
 }
--- screen_average/hls_impl_final/average_screen2.h
+++ screen_average/hls_impl_final/average_screen2.h
@@ -5,10 +5,11 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef ap_uint<8> color_array;
+
 #define IMAGE_LENGTH 255960
 //#define IMAGE_LENGTH 11
 
-int color255[256] = {0};
-
+ap_uint<64> color255[256] = {0};
 
 #endif
